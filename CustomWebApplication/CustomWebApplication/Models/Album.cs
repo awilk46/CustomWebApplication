e02a1e8008dda111cc3c09015286e98317443008
Adrian Wilk <adrian94wilk@gmail.com>
2019-10-23 13:46:04 +0200

    models

---
+++ CustomWebApplication/CustomWebApplication/Models/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomWebApplication.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public int CategoryID { get; set; }      
+        public string AlbumName { get; set; }
+        public string BandName { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public virtual ICollection<Song> Songs { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ CustomWebApplication/CustomWebApplication/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomWebApplication.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string AlbumID { get; set; }      
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal OrderPrice { get; set; }
+        public Album Album { get; set; }
+    }
+}

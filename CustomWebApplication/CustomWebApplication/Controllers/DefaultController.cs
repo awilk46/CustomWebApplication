d77ae9244d1d1a6b6ccb0ce67ffa19f9d683d3a2
Adrian Wilk <adrian94wilk@gmail.com>
2019-10-25 21:53:45 +0200

    preliminary search

--- CustomWebApplication/CustomWebApplication/Controllers/DefaultController.cs
+++ CustomWebApplication/CustomWebApplication/Controllers/DefaultController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace CustomWebApplication.Controllers
 {
@@ -39,15 +40,49 @@ namespace CustomWebApplication.Controllers
      
             return Json(songs,JsonRequestBehavior.AllowGet);
         }
-        public JsonResult SongListByAlbum2(int albumID)
-        {
-            var songs = (from song in Song.GetSongList()
-                         join album in Album.GetAlbumList()
-                         on song.AlbumID equals album.AlbumID
-                         where (albumID == album.AlbumID)
-                         select song).ToList();
 
-            return Json(new { html = songs }, JsonRequestBehavior.AllowGet);
+        public JsonResult GetSearchingData(int categoryID, string searchBy, string searchValue)
+        {
+            var albumsWithCategory = (from album in Album.GetAlbumList()
+                                      join category in Category.GetCategoriesList()
+                                      on album.CategoryID equals category.CategoryID
+                                      where (categoryID == album.CategoryID)
+                                      select album).ToList();
+            List<Album> newAlbumList;
+
+            if (searchBy == "albumID")
+            {
+                 int Id = Convert.ToInt32(searchValue);
+                 newAlbumList = albumsWithCategory.Where(x => x.AlbumID == Id || searchValue == null).ToList();
+
+
+                return Json(newAlbumList, JsonRequestBehavior.AllowGet);
+            }
+            else if (searchBy == "AlbumName")
+            {
+                newAlbumList = albumsWithCategory.Where(x => x.AlbumName.StartsWith(searchValue, StringComparison.InvariantCultureIgnoreCase) || searchValue == null).ToList();
+                return Json(newAlbumList, JsonRequestBehavior.AllowGet);
+            }
+            else if (searchBy == "BandName")
+            {
+                newAlbumList = albumsWithCategory.Where(x => x.BandName.StartsWith(searchValue, StringComparison.InvariantCultureIgnoreCase) || searchValue == null).ToList();
+                return Json(newAlbumList, JsonRequestBehavior.AllowGet);
+            }
+            else if (searchBy == "Price")
+            {
+            
+                int Pr = Convert.ToInt32(searchValue);
+                newAlbumList = albumsWithCategory.Where(x => x.Price == Pr || searchValue == null).ToList();
+                if (!int.TryParse(searchValue, out Pr))
+                {
+                    throw new FormatException();
+                }
+                return Json(newAlbumList, JsonRequestBehavior.AllowGet);
+            }
+            else {
+                return Json(albumsWithCategory, JsonRequestBehavior.AllowGet);
+            }       
         }
+
     }
 }
